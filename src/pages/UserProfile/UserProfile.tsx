03068be27286d20472a89fdff0f5d8ee63edcc57
Claudinier JC Neto <devneto576@gmail.com>
2023-03-09 09:38:03 -0300

    Merge Development

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -2,9 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import UserInfos from "../../components/UserInfos/UserInfos";
-import { CustomListContext } from "../../providers/ListCustomContext";
-import { ICustomList } from "../../providers/ListCustomContext/type";
-import { api } from "../../services/api";
 
 const UserProfile = () => {
   return (
